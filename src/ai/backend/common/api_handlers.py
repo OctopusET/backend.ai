9a715e52bd9e14119b6ef0e8468b6308e203f746
jopemachine <gbl@lablup.com>
2025-08-04 07:28:54 +0000

    feat: Add HuggingFace scanner, and API to storage proxy

--- src/ai/backend/common/api_handlers.py
+++ src/ai/backend/common/api_handlers.py
@@ -160,6 +160,8 @@ class MiddlewareParam(ABC, BaseModel):
     async def from_request(cls, request: web.Request) -> Self:
         pass
 
+    # TODO: How about adding `model_config = ConfigDict(arbitrary_types_allowed=True)` here?
+
 
 JSONDict: TypeAlias = dict[str, Any]
 
